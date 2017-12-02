df59d7226e80cd3b63197d161b910cb6837b3f57
Brandon Chong <brndnchong@gmail.com>
2017-12-02 13:19:16 -0800

    Change controller name

---
+++ api/Controllers/YoteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class YoteController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
